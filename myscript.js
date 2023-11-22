73527939d1a9d721e7c2ffe2db13edad3028cca9
GhulamAbbasZaidi <zaidiabbas17@gmail.com>
2023-11-23 00:54:45 +0500

    quiz app

--- myscript.js
+++ myscript.js
@@ -1,4 +1,4 @@
-// Define an array of quiz questions
+const start = document.getElementById("start-button");
 const quizQuestions = [
   {
     question: "What is the capital of France?",
@@ -17,57 +17,46 @@ const quizQuestions = [
   },
 ];
 
-// Variables to track quiz state
 let currentQuestionIndex = 0;
 let score = 0;
 let timeLeft = 30;
 let timerInterval;
 
-// Function to start the quiz
 function startQuiz() {
-  // Hide the start button and display the first question
   document.getElementById("start-button").style.display = "none";
   displayQuestion();
   startTimer();
 }
 
-// Function to display a question and its options
 function displayQuestion() {
   const currentQuestion = quizQuestions[currentQuestionIndex];
   const questionText = document.getElementById("question-text");
   const answerButtons = document.getElementById("answer-buttons");
 
-  // Clear previous question and answer options
   questionText.innerHTML = "";
   answerButtons.innerHTML = "";
 
-  // Display the current question
   questionText.innerHTML = currentQuestion.question;
 
-  // Create answer buttons for each option
   currentQuestion.options.forEach((option) => {
     const button = document.createElement("button");
     button.innerText = option;
     button.classList.add("answer-button");
     answerButtons.appendChild(button);
 
-    // Add click event listener to check the answer
     button.addEventListener("click", function () {
       checkAnswer(option);
     });
   });
 }
 
-// Function to check the selected answer
 function checkAnswer(selectedOption) {
   const currentQuestion = quizQuestions[currentQuestionIndex];
 
-  // Check if the selected answer is correct
   if (selectedOption === currentQuestion.correctAnswer) {
     score++;
   }
 
-  // Move to the next question or end the quiz if all questions are answered
   currentQuestionIndex++;
 
   if (currentQuestionIndex < quizQuestions.length) {
@@ -77,30 +66,23 @@ function checkAnswer(selectedOption) {
   }
 }
 
-// Function to start the timer
 function startTimer() {
   timerInterval = setInterval(function () {
     timeLeft--;
 
-    // Update the timer text
     document.getElementById("timer").textContent = timeLeft;
 
-    // End the quiz if time runs out
     if (timeLeft <= 0) {
       endQuiz();
     }
   }, 1000);
 }
 
-// Function to end the quiz
 function endQuiz() {
-  // Stop the timer
   clearInterval(timerInterval);
 
-  // Calculate the score percentage
   const scorePercentage = (score / quizQuestions.length) * 100;
 
-  // Display the final score
   const questionContainer = document.getElementById("question-container");
   questionContainer.innerHTML = `
     <h2>Quiz Completed!</h2>
@@ -109,5 +91,4 @@ function endQuiz() {
   `;
 }
 
-// Add event listener to start the quiz when the start button is clicked
-document.getElementById("start-button").addEventListener("click", startQuiz);
+start.addEventListener("click", startQuiz);
